89969e6c42a1b8d296f422dca8eca0ec6473a09b
nataliag409 <natalia212329@gmail.com>
2024-04-18 13:51:22 -0700

    I did Animations

--- Unit3GutierrezP5/Assets/Scripts/SpawnManager.cs
+++ Unit3GutierrezP5/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObsticle", startDelay, repeatRate);
     }
 
@@ -23,6 +25,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObsticle ()
     {
-        Instantiate(obsticlePrefab, spawnPos, obsticlePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obsticlePrefab, spawnPos, obsticlePrefab.transform.rotation);
+        }
+
     }
 }
